1e4ef139a42bc2edd2a79680aa511f22c54aaf0a
gaohb <herbertgo,ghb@gmail.com>
2021-09-13 19:13:39 +0800

    rocketmq - hello rocketmq

---
+++ src/main/java/com/gaohb/hello/study/springTest/rocketmq/Consumer.java
@@ -0,0 +1,18 @@
+package com.gaohb.hello.study.springTest.rocketmq;
+
+import com.gaohb.hello.study.config.base.Base;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RocketMQMessageListener(topic = "first-topic",consumerGroup = "producer-group")
+public class Consumer extends Base implements RocketMQListener<String> {
+
+    @Override
+    public void onMessage(String msg) {
+        logger.info(msg);
+    }
+
+
+}
---
+++ src/main/java/com/gaohb/hello/study/springTest/rocketmq/RocketProducer.java
@@ -0,0 +1,24 @@
+package com.gaohb.hello.study.springTest.rocketmq;
+
+
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rocket-producer")
+public class RocketProducer {
+
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
+
+    @PostMapping("/narmal/{msg}")
+    public String pushMsg(@PathVariable("msg") String msg){
+
+        rocketMQTemplate.convertAndSend("first-topic",msg);
+        return "OK";
+    }
+}
